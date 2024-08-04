c45ed94c725581bd17ddda2eb2213e9c57da6393
wostibishal <bishalwasti1@gmail.com>
2024-08-04 13:00:31 +0545

    new

---
+++ src/components/Balance.vue
@@ -0,0 +1,5 @@
+<template>
+    <h4>
+        <h1 id="balance">$0.00</h1>
+    </h4>
+</template>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,27 @@
+<template>
+    <h3>
+        History
+    </h3>
+    <ul id="list" class="list">
+        <!-- <li class="minus"> -->
+            <!-- cash<span>-$400</span> -->
+            <!-- <button class="delete-btn">x</button> -->
+        <!-- </li> -->
+        <!-- <li class="plus"> -->
+        <!-- paycheck<span>$400</span> -->
+        <!-- <button class="delete-btn">x</button> -->
+        <!-- </li> -->
+    </ul>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            transactions: [
+                {id:1, text: ''}
+            ]
+        }
+    }
+}
+</script>
